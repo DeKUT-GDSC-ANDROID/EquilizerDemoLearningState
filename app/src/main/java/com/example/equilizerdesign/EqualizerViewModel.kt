1c0685428e5ac12a0b91df84f3a094711ad1e84f
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-04-25 22:58:40 +0300

    impl saved savedStateHandle

--- app/src/main/java/com/example/equilizerdesign/EqualizerViewModel.kt
+++ app/src/main/java/com/example/equilizerdesign/EqualizerViewModel.kt
@@ -1,37 +1,45 @@
 package com.example.equilizerdesign
 
+import android.os.Parcelable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import kotlinx.parcelize.Parcelize
 
-class EqualizerViewModel: ViewModel() {
-    var uiState by mutableStateOf(UiState())
-
+//savedStateHandle["state"] = savedStateHandle.get<UiState>("state")?.copy(
+//sliderFiveValue = event.value
+//)
+class EqualizerViewModel(
+    private val savedStateHandle : SavedStateHandle
+): ViewModel() {
+//    var uiState by mutableStateOf(UiState())
+val uiState = savedStateHandle.getStateFlow("state",UiState() )
     fun onEvent(event : UiEvents){
         when(event){
             is UiEvents.AdjustingSliderFive -> {
-                uiState = uiState.copy(
+                savedStateHandle["state"] = savedStateHandle.get<UiState>("state")?.copy(
                     sliderFiveValue = event.value
                 )
             }
             is UiEvents.AdjustingSliderFour ->{
-                uiState = uiState.copy(
+                savedStateHandle["state"] = savedStateHandle.get<UiState>("state")?.copy(
                     sliderFourValue = event.value
                 )
             }
             is UiEvents.AdjustingSliderOne -> {
-                uiState = uiState.copy(
+                savedStateHandle["state"] = savedStateHandle.get<UiState>("state")?.copy(
                     sliderOneValue = event.value
                 )
             }
             is UiEvents.AdjustingSliderThree -> {
-                uiState = uiState.copy(
+                savedStateHandle["state"] = savedStateHandle.get<UiState>("state")?.copy(
                     sliderThreeValue = event.value
                 )
             }
             is UiEvents.AdjustingSliderTwo -> {
-                uiState = uiState.copy(
+                savedStateHandle["state"] = savedStateHandle.get<UiState>("state")?.copy(
                     sliderTwoValue = event.value
                 )
             }
@@ -40,10 +48,11 @@ class EqualizerViewModel: ViewModel() {
     }
 }
 
+@Parcelize
 data class UiState(
     val sliderOneValue : Float = 0f,
     val sliderTwoValue : Float = 0f,
     val sliderThreeValue : Float = 0f,
     val sliderFourValue : Float = 0f,
     val sliderFiveValue : Float = 0f,
-)
+) : Parcelable
--- app/src/main/java/com/example/equilizerdesign/EquilizerUi.kt
+++ app/src/main/java/com/example/equilizerdesign/EquilizerUi.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,7 +17,8 @@ import com.example.equilizerdesign.utils.VerticalSlider
 @Composable
 fun EqualizerUi() {
     val viewModel: EqualizerViewModel = viewModel()
-    val state = viewModel.uiState
+//    val state = viewModel.uiState
+    val state = viewModel.uiState.collectAsState().value
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()){
         EqualizerCustom(uiState = state, viewModel = viewModel)
     }
